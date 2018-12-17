ff811f14f11eb7d63f05a55ea704982b103eaa7c
Nathan Maurice <nathan.maurice@protonmail.com>
2018-12-17 12:18:17 +0100

    Ajout structure test integration border

---
+++ test_integration/tests_integration.java
@@ -0,0 +1,11 @@
+class TestBOrder {
+	public void main(String[] args) {
+		
+		IEtatPartie = FactorySUBOO.createEtatPartie();
+		IObjectif = FactorySUBOO.createObjectif();
+		IBOrder = FactorySUBOO.createBOrder();
+		
+		
+		
+	}
+}
